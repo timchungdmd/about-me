4ab60d0a9ebf5c4994d259a5cfec1f102bc4f26d
timchungdmd <timchung.dmd@gmail.com>
2022-10-05 10:40:47 -0700

    1

--- js/test.js
+++ js/test.js
@@ -1,3 +1,3 @@
 alert("Hi, welcome to my About Me page. Let me startb by asking your name.")
 let userName=prompt("What is your name?")
-alert(userName+' I\m glad you are here. I am going to ask five questions about things I may or may not like. Are you ready? Here we go!)
+alert(userName+' I\m glad you are here. I am going to ask five questions about things I may or may not like. Are you ready? Here we go!')
