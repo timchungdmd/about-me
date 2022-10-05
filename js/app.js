be91bb35523f95aeb24b07df96a1f048372a2c27
timchungdmd <timchung.dmd@gmail.com>
2022-10-04 21:53:00 -0700

    modified:   js/app.js

--- js/app.js
+++ js/app.js
@@ -36,7 +36,6 @@ One true and it will run
 //if (trueValue && falseValue){
 //  console.log('it evaluated to true');
 //}
-function fiveQuestions() {
   let username = prompt("What is your name?");
   console.log(username);
   let greet = alert("Hi " + username + ", welcome to my About Me page");
@@ -44,16 +43,17 @@ function fiveQuestions() {
   let likeMarvel = prompt("Do you like Marvel movies?");
   likeMarvel = likeMarvel.toLowerCase();
   //console.log(likeMarvel);
-
   if (likeMarvel === "yes" || likeMarvel === "y") {
     //console.log('So do I! It is my favorite movie franchise');
     alert("So do I! It is my favorite movie franchise");
-  } else if (likeMarvel === "no" || likeMarvel === "n") {
+  } else if(likeMarvel === "no" || likeMarvel === "n") {
     //console.log(('It\'s ok, to each its own');
     alert("It's ok, to each its own");
+  }else {
+    console.log('You need to answer with a Yes or a No');
   }
 
-  likeTacos = prompt("Do you like tacos?");
+  let likeTacos = prompt("Do you like tacos?");
   likeTacos = likeTacos.toLowerCase();
   if (likeTacos === "yes" || likeTacos === "y") {
     //console.log('Taco is my favorite food, I can probably eat it 24/7');
@@ -61,9 +61,12 @@ function fiveQuestions() {
   } else if (likeTacos === "no" || likeTacos === "n") {
     //console.log('It was never a right or wrong question.You do you!');
     alert("It was never a right or wrong question. You do you!");
+  }else {
+    console.log('You need to answer with a Yes or a No');
   }
+ 
 
-  likeAnime = prompt("Do you watch animes?");
+  let likeAnime = prompt("Do you watch animes?");
   likeAnime = likeAnime.toLowerCase();
   if (likeAnime === "yes" || likeAnime === "y") {
     //console.log('Me too, my favorite one is One Piece');
@@ -71,9 +74,11 @@ function fiveQuestions() {
   } else if (likeAnime === "no" || likeAnime === "n") {
     //console.log('There are so many things to watch these days!');
     alert("There are so many things to watch these days!");
+  }else {
+    console.log('You need to answer with a Yes or a No');
   }
 
-  likeApple = prompt("Do you have an iPhone?");
+  let likeApple = prompt("Do you have an iPhone?");
   likeApple = likeApple.toLowerCase();
   if (likeApple === "yes" || likeApple === "y") {
     //console.log('Me too, Apple ecosystem is pretty amazing');
@@ -81,9 +86,11 @@ function fiveQuestions() {
   } else if (likeApple === "no" || likeApple === "n") {
     //console.log('Android is ok, I guess...!');
     alert("Android is ok, I guess...!");
+  }else {
+    console.log('You need to answer with a Yes or a No');
   }
 
-  likeTravel = prompt("Have you traveled anywhere in the past 6 months?");
+  let likeTravel = prompt("Have you traveled anywhere in the past 6 months?");
   likeTravel = likeTravel.toLowerCase();
   if (likeTravel === "yes" || likeTravel === "y") {
     //console.log('Me too, it\s a great way to take a break and recharge mentally and physically');
@@ -93,7 +100,8 @@ function fiveQuestions() {
   } else if (likeTravel === "no" || likeTravel === "n") {
     //console.log('You should plan for a travel in the near future.');
     alert("You should plan for a travel in the near future.");
+  }else {
+    console.log('You need to answer with a Yes or a No');
   }
 
   alert(username + ", thank you for participating in this short survey!");
-}
