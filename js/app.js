9a36b175a7be139947f1d2c3e84bbe3ce8ababbb
timchungdmd <timchung.dmd@gmail.com>
2022-10-05 10:22:36 -0700

    console.log to alert

--- js/app.js
+++ js/app.js
@@ -15,17 +15,17 @@ One true and it will run
 //}
   let username = prompt("What is your name?");
   let greet = alert("Hi " + username + ", welcome to my About Me page");
+
   let likeMarvel = prompt("Do you like Marvel movies?");
   likeMarvel = likeMarvel.toLowerCase();
-  //console.log(likeMarvel);
   if (likeMarvel === "yes" || likeMarvel === "y") {
     //console.log('So do I! It is my favorite movie franchise');
     alert("So do I! It is my favorite movie franchise");
   } else if(likeMarvel === "no" || likeMarvel === "n") {
     //console.log(('It\'s ok, to each its own');
     alert("It's ok, to each its own");
-  }else {
-    console.log('You need to answer with a Yes or a No');
+  }else{
+    alert('You need to answer with a Yes or a No');
   }
 
   let likeTacos = prompt("Do you like tacos?");
@@ -37,7 +37,7 @@ One true and it will run
     //console.log('It was never a right or wrong question.You do you!');
     alert("It was never a right or wrong question. You do you!");
   }else {
-    console.log('You need to answer with a Yes or a No');
+    alert('You need to answer with a Yes or a No');
   }
  
 
@@ -50,7 +50,7 @@ One true and it will run
     //console.log('I understand. There are so many other things to watch these days!');
     alert("I understand. There are so many other things to watch these days!");
   }else {
-    console.log('You need to answer with a Yes or a No');
+    alert('You need to answer with a Yes or a No');
   }
 
   let likeApple = prompt("Do you have an iPhone?");
@@ -62,7 +62,7 @@ One true and it will run
     //console.log('Android is ok, I guess...!');
     alert("Android is ok, I guess...!");
   }else {
-    console.log('You need to answer with a Yes or a No');
+    alert('You need to answer with a Yes or a No');
   }
 
   let likeTravel = prompt("Have you traveled anywhere in the past 6 months?");
@@ -76,7 +76,7 @@ One true and it will run
     //console.log('You should plan for a travel in the near future.');
     alert("You should plan for a travel in the near future.");
   }else {
-    console.log('You need to answer with a Yes or a No');
+    alert('You need to answer with a Yes or a No');
   }
 
   alert(username + ", thank you for participating in this short survey!");
