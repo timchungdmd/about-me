b28711cc40c1f4483f694f6b4ff8eb3049ae708e
timchungdmd <timchung.dmd@gmail.com>
2022-10-05 04:23:07 -0700

    modified:   js/app.js

--- js/app.js
+++ js/app.js
@@ -118,7 +118,7 @@ let likeTacos=prompt('Do you like tacos?');
 likeTacos=likeTacos.toLowerCase();
 if(likeTacos=== 'yes' || likeTacos=== 'y'){
  //console.log('Taco is my favorite food, I can probably eat it 24/7');
- alert('Taco is my favorite food, I can probably eat it 24/7');
+ alert('Taco is my favorite food, I can eat it 24/7');
 }else if(likeTacos=== 'no' || likeTacos=== 'n'){
  //console.log('It was never a right or wrong question.You do you!');
  alert('It was never a right or wrong question. You do you!');
@@ -128,7 +128,7 @@ let likeAnime=prompt('Do you watch animes?');
 likeAnime=likeAnime.toLowerCase();
 if(likeAnime==='yes'|| likeAnime==='y'){
   //console.log('Me too, my favorite one is One Piece');
-  alert('Me too, my favorite one is One Piece');
+  alert('Me too! My favorite anime is One Piece');
 }else if(likeAnime==='no'||likeAnime==='n'){
   //console.log('There are so many things to watch these days!');
   alert('There are so many things to watch these days!');
@@ -138,7 +138,7 @@ let likeApple=prompt('Do you have an iPhone?');
 likeApple=likeApple.toLowerCase();
 if(likeApple==='yes'|| likeApple==='y'){
   //console.log('Me too, Apple ecosystem is pretty amazing');
-  alert('Me too, Apple ecosystem is pretty amazing');
+  alert('Me too! Apple ecosystem is pretty amazing');
 }else if(likeApple==='no'||likeApple==='n'){
   //console.log('Android is ok, I guess...!');
   alert('Android is ok, I guess...!');
@@ -148,7 +148,7 @@ let likeTravel=prompt('Have you traveled anywhere in the past 6 months?');
 likeTravel=likeTravel.toLowerCase();
 if(likeTravel==='yes'|| likeTravel==='y'){
   //console.log('Me too, it\s a great way to take a break and recharge mentally and physically');
-  alert('Me too, it\s a great way to take a break and recharge mentally and physically');
+  alert('Me too! It\s a great way to take a break and recharge mentally and physically');
 }else if(likeTravel==='no'||likeTravel==='n'){
   //console.log('You should plan for a travel in the near future.');
   alert('You should plan for a travel in the near future.');
