1ec0406f7a4d8d9a088c6737daac7212e2aebbdf
timchungdmd <timchung.dmd@gmail.com>
2022-10-05 20:27:17 -0700

    modified:   js/test.js

--- js/test.js
+++ js/test.js
@@ -38,8 +38,8 @@ const random = Math.floor(Math.random() * 7) + 1;
 let answer=mcdmenu[random]
 let tries1=6;
 for(let i=6; i>0; i--){
-  console.log("MENU: "+mcdmenu);
-  let guess=prompt(`Guess my favorite menu from McDonalds. You have ${tries1} guesses.`)
+  let menu=console.log("MENU: "+mcdmenu);
+  guess=prompt(`Guess my favorite menu from McDonalds. You have ${tries1} guesses.`+menu);
   let guessUp=guess.toUpperCase()
   
   if(guessUp===answer){
